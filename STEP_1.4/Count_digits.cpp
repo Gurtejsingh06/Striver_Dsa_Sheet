873968eb1e6f35a950329cd28e877ae8bf096d84
Gurtej Singh <40589400+Gurtejsingh06@users.noreply.github.com>
2022-10-02 23:54:09 +0530

    Update Count_digits.cpp

--- STEP_1.4/Count_digits.cpp
+++ STEP_1.4/Count_digits.cpp
@@ -1,3 +1,5 @@
+// My Solution
+
 #include <bits/stdc++.h>
 
 #define ll long long
@@ -32,6 +34,46 @@ int main()
             N /= 10;
             size--;
         }
+        cout << count;
+
+
+	return 0;
+}
+
+// Autor Solution 
+
+#include <bits/stdc++.h>
+
+#define ll long long
+#define f first
+#define s second
+#define ull unsigned long long int
+#define pushb push_back
+#define popb pop_back
+#define INF 1e18+9
+#define endl '\n'
+#define Mod 1000000007
+#define MAX 1000001
+
+using namespace std;
+
+int main()
+{
+	ll N=0;
+	cin >> N;
+
+	int temp = N, count = 0;
+        while (temp != 0)
+        {
+            // extract the last digit
+            int d = temp % 10;
+            temp /= 10;
+             
+            // check if d divide N completly
+            if (d > 0 && N%d == 0)
+              count++;
+        }
+
         cout << count;
 
 
