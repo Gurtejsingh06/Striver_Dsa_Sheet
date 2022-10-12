f70cc1c799922a8ae3f173aadd3801ac585efff4
Gurtej Singh <40589400+Gurtejsingh06@users.noreply.github.com>
2022-10-12 22:56:12 +0530

    Create Sum of first N numbers.cpp

---
+++ STEP_1.5/Sum of first N numbers.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define f first
+#define s second
+#define ull unsigned long long int
+#define pushb push_back
+#define popb pop_back
+#define INF 1e18+9
+#define endl '\n'
+#define Mod 1000000007
+#define MAX 1000001
+
+using namespace std;
+void SumOfN(ll N){
+        ll sum = N*(N+1)/2;
+        cout << sum;
+}
+int main() {
+
+  ll n;
+  cin >> n;
+  SumOfN(n);
+  return 0;
+}
+
+
